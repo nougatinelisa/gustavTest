b1e17b6e8efcbe0bedf9beab85f2fd75231a2341
Sarah <sarah.drapeau@hotmail.fr>
2017-07-28 12:53:36 +0200

    add role option and acl

--- server/boot/role.js
+++ server/boot/role.js
@@ -1,23 +1,44 @@
 module.exports = function(app) {
-    var User = app.models.user;
-    var role = app.models.Role;
-    var RoleMapping = app.models.roleMapping;
+    var User = app.models.utilisateur;
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+//
+//     User.create([
+//         {username: 'Jane', email: 'jane@doe.com', password: 'root'}
+//     ], function(err, users) {
+//         if (err) throw err;
+//
+//     //create the superAdmin role
+//     role.create({
+//         name: 'superAdmin'
+//     }, function(err, role) {
+//         if (err) throw err;
+//
+//         console.log('Created role:', role)
+//     });
+//
+//     //create the admin role
+//     role.create({
+//         name: 'admin'
+//     }, function(err, role) {
+//         if (err) throw err;
+//
+//         console.log('Created role:', role);
+//
+//         //make user with id=2 an admin
+//         role.principals.create({
+//             principalType: RoleMapping.USER,
+//             principalId: users[4]
+//         }, function(err, principal) {
+//             if (err) throw err;
+//
+//             console.log('Created principal:', principal);
+//         });
+//     });
+//     })
+// };
+
+    RoleMapping.belongsTo(User);
+    User.hasMany(RoleMapping, {foreignKey: 'principalId'});
+    Role.hasMany(User, {through: RoleMapping, foreignKey: 'roleId'});
 };
-
-//create the superAdmin role
-role.create({
-    name: 'superAdmin'
-}, function(err, role) {
-    if (err) throw err;
-
-    console.log('Created role:', role)
-});
-
-//create the admin role
-role.create({
-    name: 'admin'
-}, function(err, role) {
-    if (err) throw err;
-
-    console.log('Created role:', role)
-});
