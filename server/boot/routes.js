353468f49615e1f1e9bdca32df497efdffd1bc9d
Sarah <sarah.drapeau@hotmail.fr>
2017-08-02 12:01:47 +0200

    add localStorage

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,6 +1,11 @@
 module.exports = function(app) {
     var router = app.loopback.Router();
 
+    if (typeof localStorage === "undefined" || localStorage === null) {
+        var LocalStorage = require('node-localstorage').LocalStorage;
+        localStorage = new LocalStorage('./scratch');
+    }
+
     router.get('/', function (req, res) {
         res.render('index');
     });
@@ -27,45 +32,40 @@ module.exports = function(app) {
                     });
                 }
 
-
                 token = token.toJSON();
-                var accessToken = encodeURIComponent(token.id);
+                var accessToken = token.id;
 
+                localStorage.setItem('accessToken', accessToken);
 
-                // localStorage.setItem('accessToken', accessToken);
-
-                res.redirect('/admin?access='+ accessToken);
+                res.redirect('/admin');
             });
         });
 
     router.get('/admin', function (req, res) {
 
         res.render('home', {
-            // username: req.token.utilisateur.username,
-                accessToken: req.query.access
+                accessToken: localStorage.getItem('accessToken')
         });
-
-        // console.log(window.localStorage.getItem('accessToken'));
     });
 
-    router.get('/logout', function(req, res) {
-        var AccessToken = app.models.AccessToken;
-        var token = new AccessToken({id: req.query['access_token']});
-        token.destroy();
-
-        // localStorage.setItem('accessToken', null);
-
-        res.redirect('/');
-    });
-
-    // //log a user out
-    // app.get('/logout', function(req, res, next) {
-    //     if (!req.accessToken) return res.sendStatus(401); //return 401:unauthorized if accessToken is not present
-    //     User.logout(req.accessToken.id, function(err) {
-    //         if (err) return next(err);
-    //         res.redirect('/'); //on successful logout, redirect
-    //     });
+    // router.get('/logout', function(req, res) {
+    //     var AccessToken = app.models.AccessToken;
+    //     var token = new AccessToken({id: localStorage.getItem('accessToken')});
+    //     token.destroy();
+    //
+    //     localStorage.removeItem('accessToken');
+    //
+    //     res.redirect('/');
     // });
+
+    //log a user out
+    router.get('/logout', function(req, res, next) {
+        if (!localStorage.getItem('accessToken')) return res.sendStatus(401); //return 401:unauthorized if accessToken is not present
+        app.models.utilisateur.logout(localStorage.getItem('accessToken'), function(err) {
+            if (err) return next(err);
+            res.redirect('/'); //on successful logout, redirect
+        });
+    });
 
     app.use(router);
 
