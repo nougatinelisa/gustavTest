e27da781d8859ef0a534950671771b6f71969d6f
Sarah <sarah.drapeau@hotmail.fr>
2017-08-04 09:30:17 +0200

    add roots and views for holocube

--- server/boot/routes.js
+++ server/boot/routes.js
@@ -1,21 +1,28 @@
+// All variables that will be used again
 module.exports = function(app) {
     var router = app.loopback.Router();
+    var holocube = app.models.holocube;
+    var utilisateur = app.models.utilisateur;
+    var playlist = app.models.playlist;
 
     if (typeof localStorage === "undefined" || localStorage === null) {
         var LocalStorage = require('node-localstorage').LocalStorage;
         localStorage = new LocalStorage('./scratch');
     }
 
+    //index of the website
     router.get('/', function (req, res) {
         res.render('index');
     });
 
+    // login page
     router.get('/login', function(req, res) {
         res.render('login', {
             loginFailed: false
         });
     });
 
+    // page where you send the data and connect the user
     router.post('/adminLogin', function(req, res) {
         var email = req.body.email;
         var password = req.body.password;
@@ -41,22 +48,120 @@ module.exports = function(app) {
             });
         });
 
+    // homepage of the back office
     router.get('/admin', function (req, res) {
-
-        res.render('home', {
+        if (!localStorage.getItem('accessToken')) {
+            return res.sendStatus(401);
+        }
+        res.render('back/home', {
                 accessToken: localStorage.getItem('accessToken')
         });
     });
 
-    // router.get('/logout', function(req, res) {
-    //     var AccessToken = app.models.AccessToken;
-    //     var token = new AccessToken({id: localStorage.getItem('accessToken')});
-    //     token.destroy();
-    //
-    //     localStorage.removeItem('accessToken');
-    //
-    //     res.redirect('/');
-    // });
+//\\//\\    HOLOCUBE    //\\//\\
+
+    //index page
+    router.get('/admin/holocube', function (req, res) {
+        if (!localStorage.getItem('accessToken')) {
+            return res.sendStatus(401);
+        }
+
+        holocube.find({}, function (err, holocubes) {
+            if(err) {}
+
+            res.render('back/hologramme/index', {
+                holocubes: holocubes,
+                empty: false
+            })
+        });
+    });
+
+    // new holocube
+    router.get('/admin/holocube/new', function (req, res) {
+        if (!localStorage.getItem('accessToken')) {
+            return res.sendStatus(401);
+        }
+
+        utilisateur.find({}, function (err, utilisateurs) {
+            if(err) {}
+
+            playlist.find({}, function (err, playlists) {
+                if(err) {}
+
+                return res.render('back/hologramme/new', {
+                    err: false,
+                    utilisateurs: utilisateurs,
+                    playlists: playlists
+                });
+            });
+        });
+    });
+
+    // new holocube when you send the POST data
+    router.post('/admin/holocube/new', function (req, res) {
+        if (!localStorage.getItem('accessToken')) {
+            return res.sendStatus(401);
+        }
+
+        var nom = req.body.nom;
+        var adresseIP = req.body.adresseIP;
+        var mdp = req.body.password;
+        var client = req.body.client;
+        var playlist = req.body.playlist;
+
+        holocube.create({
+            nomHolocube: nom,
+            adresseIP: adresseIP,
+            mdp: mdp,
+            utilisateurId: client,
+            playlistId: playlist
+        }, function (err, holocube) {
+            if(err) {
+                return res.render('login', {
+                    nom: nom,
+                    adresseIP: adresseIP,
+                    mdp: mdp,
+                    client: client,
+                    playlist: playlist,
+                    err: true
+                });
+            }
+            res.redirect('/admin/holocube')
+        })
+    });
+
+    // edit holocube
+    router.get('/admin/holocube/:id', function (req, res) {
+        if (!localStorage.getItem('accessToken')) {
+            return res.sendStatus(401);
+        }
+        var holocubeId = req.params.id;
+        holocube.findById(holocubeId, function (err, holocube) {
+            if(err) throw err;
+
+            utilisateur.find({}, function (err, utilisateurs) {
+                if(err) {}
+
+                playlist.find({}, function (err, playlists) {
+                    if(err) {}
+
+                    return res.render('back/hologramme/edit', {
+                        err: false,
+                        holocube: holocube,
+                        utilisateurs: utilisateurs,
+                        playlists: playlists
+                    });
+                });
+            });
+        });
+    });
+
+    //edit holocube when you send the POST data
+    router.post('/admin/holocube/:id', function (req, res) {
+        if (!localStorage.getItem('accessToken')) {
+            return res.sendStatus(401);
+        }
+    });
 
     //log a user out
     router.get('/logout', function(req, res, next) {
